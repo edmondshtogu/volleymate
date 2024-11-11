ca67f22098d15698ceb660b3d9c150ed59f200fd
edmondshtogu <edmondshtogu@gmail.com>
2024-11-12 00:29:45 +0100

    adding image link

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -20,7 +20,7 @@ const whatsapp = {
           header: {
             type: "image",
             image: {
-              id: "2762702990552401",
+              link: "https://scontent.whatsapp.net/v/t61.29466-34/465210699_1276700240134274_8954537288700425218_n.png?ccb=1-7&_nc_sid=8b1bef&_nc_ohc=Tm92z77LNjoQ7kNvgGYXo1v&_nc_zt=3&_nc_ht=scontent.whatsapp.net&_nc_gid=AykfzBM6o7APsfpILoM8WbJ&oh=01_Q5AaIBZdDxzVcTm2gvKixKyjuf26bKgOFHURvwgbO57951wv&oe=675A15DC",
             },
           },
           body: {
